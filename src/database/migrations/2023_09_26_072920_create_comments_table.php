e8fa970bad32429e20bdcb9c1679111fd287465d
ouzyu <murahiro927@gmail.com>
2023-09-26 18:29:04 +0900

    [modelとmigrationファイルの作成]

---
+++ src/database/migrations/2023_09_26_072920_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->comment('ユーザーID');
+            $table->foreignId('tweet_id')->constrained()->comment('ツイートID');
+            $table->string('text')->comment('コメント本文');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index(['user_id', 'tweet_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ src/database/migrations/2023_09_26_073029_create_favorites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->comment('ユーザーID');
+            $table->foreignId('tweet_id')->constrained()->comment('ツイートID');
+
+            $table->unique(['user_id', 'tweet_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
